d267e7535711b06ae2bf1df955b4792032daa0c2
Jack Krause <jmkrause@iastate.edu>
2023-11-04 00:02:46 -0500

    commit

--- store_app/src/index.js
+++ store_app/src/index.js
@@ -31,9 +31,30 @@ function AppHeader() {
 function InitialAppBody() {
   return (
     <>
-    <nav>
-      <h1>Hello, there</h1>
-      </nav>
+<div className="container marketing">
+
+{/* Three columns of text below the carousel */}
+<div className="row">
+  <div className="col-lg-4">
+    <h2 className="fw-normal">Featured: Is Jaws Real?</h2>
+    <p>Believe it or not, Jaws is actually based on a true story. The movie roughly follows the events of the
+      Jersey Shore shark attack of 1916</p>
+    <p><a className="btn btn-secondary" href="./action.html">More Info &raquo;</a></p>
+  </div>{/* /.col-lg-4 */}
+  <div className="col-lg-4">
+    <h2 className="fw-normal">Featured: <strong>Will Ferrell</strong></h2>
+    <p>John William Ferrell is 56 years of age. Throughout his career, he's earned four Emmy Awards and the Mark
+      Twain Prize for American Humor in 2011.</p>
+    <p><a className="btn btn-secondary" href="./comedy.html">View Comedies &raquo;</a></p>
+  </div>{/* /.col-lg-4 */}
+  <div className="col-lg-4">
+    <h2 className="fw-normal">Featured: Is 'Johnny Football' kid-friendly?</h2>
+    <p>Untold: Johnny Football is rated TV-MA and features explicit language. You may want to think twice before
+      showing your kids this movie!</p>
+    <p><a className="btn btn-secondary" href="./documentary.html">View All Documentaries &raquo;</a></p>
+  </div>{/* /.col-lg-4 */}
+</div>{/* /.row */}
+</div>
     </>
   );
 }
