e947a9b463a81f8b4e9c24e7f769a3f7c158b098
Jack Krause <jmkrause@iastate.edu>
2023-11-03 23:57:58 -0500

    commit

--- store_app/src/index.js
+++ store_app/src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function AppHeader() {
   return (
-    <div>
+    <>
       <header data-bs-theme="dark">
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">Home</a>
+            <h1 className="navbar-brand">Home</h1>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
               aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
@@ -16,25 +17,44 @@ function AppHeader() {
             <div className="collapse navbar-collapse" id="navbarCollapse">
               <ul className="navbar-nav me-auto mb-2 mb-md-0">
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Products</a>
+                  <h2 className="nav-link">Products</h2>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
       </header>
-    </div>
+    </>
   );
 }
 
+function InitialAppBody() {
+  return (
+    <>
+    <nav>
+      <h1>Hello, there</h1>
+      </nav>
+    </>
+  );
+}
+
+function Initial() {
+  return (
+    <>
+      <AppHeader />
+      <InitialAppBody />
+    </>
+  );
+}
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
   <React.StrictMode>
-    <AppHeader />
+    <Initial />
   </React.StrictMode>
 );
+
 
 
