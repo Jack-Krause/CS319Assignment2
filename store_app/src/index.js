1f4971711b857135515040d09becaf50e3b1a7ed
Jack Krause <jmkrause@iastate.edu>
2023-11-03 23:23:33 -0500

    compiles

--- store_app/src/index.js
+++ store_app/src/index.js
@@ -5,17 +5,24 @@ import "./index.css";
 function AppHeader() {
   return (
     <div>
-      <header>
-        <h1>Header</h1>
+      <header data-bs-theme="dark">
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Home</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+              aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Products</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </header>
-    </div>
-  );
-}
-
-function TestRen() {
-  return (
-    <div>
-      <h1>Test Hello There</h1>
     </div>
   );
 }
@@ -24,10 +31,10 @@ function TestRen() {
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//   root.render(
-//   <React.StrictMode>
-//     <TestRen />
-//   </React.StrictMode>
-// );
+  root.render(
+  <React.StrictMode>
+    <AppHeader />
+  </React.StrictMode>
+);
 
 
